e08e2b9e88e701f9ba3317aafde52f20f88dbd11
Valeria Lapshina <lapshina_valeria@mail.ru>
2024-10-25 23:17:22 +0300

    Initial commit

---
+++ app/src/main/java/com/example/androidlab1/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.androidlab1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Button send = findViewById(R.id.button);
+        send.setOnClickListener(this::sendGreeting);
+    }
+
+    public void sendGreeting(View view) {
+        Intent nameIntent = new Intent(this, NameActivity.class);
+        EditText greetingField = findViewById(R.id.greetingField);
+        String greeting = greetingField.getText().toString();
+        nameIntent.putExtra("message", greeting);
+        startActivityForResult(nameIntent, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode != RESULT_OK || data == null)
+            return;
+        String greetingAndName = data.getStringExtra("message");
+        TextView greetingAndNameView = findViewById(R.id.greetingAndName);
+        greetingAndNameView.setText(greetingAndName);
+    }
+}
---
+++ app/src/main/java/com/example/androidlab1/NameActivity.java
@@ -0,0 +1,43 @@
+package com.example.androidlab1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class NameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_name);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Intent intent = getIntent();
+        String greeting = intent.getStringExtra("message");
+        TextView greetingView = findViewById(R.id.greeting);
+        greetingView.setText(greeting);
+
+        Button send = findViewById(R.id.button);
+        send.setOnClickListener(x -> {
+            Intent result = new Intent();
+            EditText nameField = findViewById(R.id.nameField);
+            String name = nameField.getText().toString();
+            result.putExtra("message", greeting + ", " + name + '!');
+            setResult(RESULT_OK, result);
+            finish();
+        });
+    }
+}
